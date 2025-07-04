791faac4d8310115d0f964dccd9a0277e892bc89
Everton002 <everton.medeiros@escola.pr.gov.br>
2025-07-04 13:16:49 -0300

    Alternativa

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principla");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1"],
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1"],
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+];
